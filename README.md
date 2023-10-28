1f18a1fbe015ce824d95d055880708b6b54afa16
Evandro P. Santos <evandropsantos@gmail.com>
2023-10-27 22:47:47 -0300

    chore: utilizando orientacao a objetos

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # py-extrator-url
+
+- [Expressões Regulares HOWTO](https://docs.python.org/pt-br/3/howto/regex.html#regex-howto)
+- [Python: Qual a diferença entre == e is?](https://www.alura.com.br/artigos/qual-a-diferenca-entre-e-is-no-python?_gl=1*l8zkxd*_ga*Nzc3MzI5MjEuMTY5NTA2NDY2Ng..*_ga_1EPWSW3PCS*MTY5ODQ1NTk4MS4xMTkuMS4xNjk4NDU3MDA5LjAuMC4w*_fplc*VW9hbEFpS0lmM0lvRVhOM01QbVNFWktSbWt6b0xSUE9GenF3T2hQN1RYQUtjUyUyRm9FdEElMkJBZFRrYyUyRjAxcVk2OSUyQmM0aSUyRlh4biUyRkNObyUyQlkzeldReHNNRW1TNVBKdDVqTGRkNm9xWG1lRkdwbDU5V2N2czN2dGlsZFFzMkN6OUElM0QlM0Q.)
+
+## O método dir
+O método dir(objeto) é um método embutido (built-in) que retorna uma lista de strings com todos os métodos e atributos do objeto passado.
+
+Por exemplo, se fizermos dir(str) temos o seguinte retorno (simplifiquei devido a grande quantidade de métodos e atributos da classe str):
+
+```python
+    dir(str)
+    # [..., ‘__len__’, ... , ‘strip’, … ‘]
+```
+Inclusive, se você souber o nome do método que está procurando, pode verificar se ele está nessa lista utilizando o operador in:
+
+```python
+    ‘__len__’ in dir(str)  # Verifica se o método __len__ existe na classe str
+    # True
+```
--- main.py
+++ main.py
@@ -1,22 +1,18 @@
-url = "https://bytebank.com/cambio" \
-    "?moedaOrigem=real" \
-    "&moedaDestino=dolar" \
-    "&quantidade=100"
+from extrator_url import ExtratorURL
 
-# Separa base e parâmetros
-indice_interrogacao = url.find('?')
-url_base = url[:indice_interrogacao]
-url_parametros = url[indice_interrogacao+1:]
-print(url_parametros)
 
-# Busca o valor de um parâmetro
-parametro_busca = 'quantidade'
-indice_parametro = url_parametros.find(parametro_busca)
-indice_valor = indice_parametro + len(parametro_busca) + 1
-indice_e_comercial = url_parametros.find('&', indice_valor)
+VALOR_DOLAR = 5.50  # 1 dólar = 5.50 reais
+URL = f"bytebank.com/cambio? \
+        quantidade=100& \
+        moedaOrigem=real \
+        &moedaDestino={VALOR_DOLAR}"
 
-valor = url_parametros[indice_valor:] \
-    if indice_e_comercial == - 1 \
-    else url_parametros[indice_valor:indice_e_comercial]
+extrator_url = ExtratorURL(URL)
 
-print(valor)
+moeda_origem = extrator_url.get_valor_parametro("moedaOrigem")
+moeda_destino = extrator_url.get_valor_parametro("moedaDestino")
+quantidade = extrator_url.get_valor_parametro("quantidade")
+
+print(moeda_origem)
+print(moeda_destino)
+print(quantidade)
